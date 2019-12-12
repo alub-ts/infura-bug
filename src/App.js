b7c8e2950f5b463bd9b743425337e83a43cf6944
alvin lubetkin <alvin@terminal.co>
2019-12-12 14:30:39 -0500

    first commit

--- src/App.js
+++ src/App.js
@@ -1,24 +1,32 @@
-import React from 'react';
-import logo from './logo.svg';
-import './App.css';
+import React from "react";
+import Web3 from "web3";
+import { TerminalHttpProvider, SourceType } from "@terminal-packages/sdk";
+
+const prodAPI = "Z0CsA9B5xAkCjfw0kcKh6g==";
+const prodProjectID = "RAqPOBVGxWjYGedk";
+
+const web3 = new Web3(
+  new TerminalHttpProvider({
+    apiKey: prodAPI,
+    projectId: prodProjectID,
+    source: "Infura",
+    host: "https://ethereum-rpc.trustwalletapp.com"
+  })
+);
 
 function App() {
+  const test = () => {
+    web3.eth.getAccounts((err, accounts) =>
+      web3.eth.sendTransaction({
+        from: accounts[0],
+        to: accounts[0],
+        value: web3.utils.toWei("0", "ether")
+      })
+    );
+  };
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <button onClick={() => test()}>Test Button</button>
     </div>
   );
 }
